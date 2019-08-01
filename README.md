54b81ae81a5ea7fbfd404a2d4aab50f46a6c2724
yutsukushi <ykushibi@gmail.com>
2019-07-31 22:32:34 -0400

    heroku jawsdb setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Burger-Logger
+
+The NodeJS-Handlebars app that takes an input for a burger you want to eat and lists it in a menu! If you want to eat it, just click "devour" and it'll be eaten.
+
+**Languages/Libraries Used**
+* JavaScript
+* MySQL
+* BootStrap
+* jQuery
+* Handlebars
+* Express
+
--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,10 @@
 // Setting up MySQL connection
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,6 +12,8 @@ var connection = mysql.createConnection({
     password: "password",
     database: "burgers_db"
 });
+}
+
 
 // Make connection
 connection.connect(function(err) {
